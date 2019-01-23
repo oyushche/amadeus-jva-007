f651dd0594fb385b9ac9f740f93506e9d53e1dda
Artem DZUBENKO <artem.dzubenko@amadeus.com>
2019-01-23 19:20:21 +0200

    In progress

--- src/com/amadeus/birds/BirdShop.java
+++ src/com/amadeus/birds/BirdShop.java
@@ -1,12 +1,17 @@
 package com.amadeus.birds;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BirdShop {
     private HashMap<BirdType, Integer> stock;
+    private ArrayList<Transaction> transactions;
 
     public BirdShop() {
         this.stock = new HashMap<>();
+        this.transactions = new ArrayList<>();
     }
 
     public void setStock(BirdType product, int count) {
@@ -23,5 +28,40 @@ public class BirdShop {
 
     public void reduceStock(BirdType product, int count) {
         this.stock.put(product, this.stock.get(product) - count);
+    }
+
+    public boolean buy(String client, BirdType product, int count)
+    {
+        if (!this.inStock(product, count)) {
+            return false;
+        }
+
+        this.transactions.add(new Transaction(client, product, count));
+        this.reduceStock(product, count);
+
+        return true;
+    }
+
+    public void printStats() {
+        HashMap<Pair<String, BirdType>, Integer> stats = new HashMap<>();
+
+        for (Transaction transaction: this.transactions) {
+            Pair<String, BirdType> key = new Pair<>(transaction.getClient(), transaction.getProduct());
+            int value = transaction.getCount();
+
+            if (stats.containsKey(key)) {
+                value += stats.get(key);
+            }
+
+            stats.put(key, value);
+        }
+
+        for (HashMap.Entry<Pair<String, BirdType>, Integer> item: stats.entrySet()) {
+            System.out.println(
+                item.getKey().getKey() + ": "
+                + item.getValue().toString() + " "
+                + item.getKey().getValue() + "'s"
+            );
+        }
     }
 }
--- src/com/amadeus/birds/Main.java
+++ src/com/amadeus/birds/Main.java
@@ -6,5 +6,13 @@ public class Main {
 
         shop.setStock(BirdType.DUCK, 2);
         shop.setStock(BirdType.EAGLE, 10);
+
+        shop.buy("Vasya", BirdType.EAGLE, 2);
+        shop.buy("Petya", BirdType.DUCK, 1);
+        shop.buy("Vasya", BirdType.DUCK, 1);
+        shop.buy("Petya", BirdType.DUCK, 1);
+        shop.buy("Vasya", BirdType.EAGLE, 5);
+
+        shop.printStats();
     }
 }
