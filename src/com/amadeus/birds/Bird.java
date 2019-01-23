0114bb5a85ad6b30dc19997a78fda77d2743c6da
Anton <anton.chernik@amadeus.com>
2019-01-23 20:22:57 +0200

    Shopping cart initialized

--- src/com/amadeus/birds/Bird.java
+++ src/com/amadeus/birds/Bird.java
@@ -1,12 +1,21 @@
 package com.amadeus.birds;
 
+import java.util.UUID;
+
 public class Bird implements IBird {
 
     public Bird(String type, double price) {
         this.type = type;
         this.price = price;
+        this.uuid = UUID.randomUUID().toString();
     }
 
+    private String uuid;
+
+
+    public String getUuid() {
+        return uuid;
+    }
 
     private String type;
 
---
+++ src/com/amadeus/birds/ShoppingCart.java
@@ -0,0 +1,4 @@
+package com.amadeus.birds;
+
+public class ShoppingCart {
+}
