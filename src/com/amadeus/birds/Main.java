26c272a83629d6a57535199a8e2d5ad834879305
Kysliakovskyi <kostik-k@ukr.net>
2019-01-24 17:33:08 +0200

    2nd

--- src/com/amadeus/birds/Main.java
+++ src/com/amadeus/birds/Main.java
@@ -6,6 +6,16 @@ public class Main
     {
         System.out.println("=====  Bird shop ====");
         BirdShopDemo birdShop = new BirdShopDemo();
+        Bird eagle = new Bird("eagle");
+        Bird albatross = new Bird("albatross");
+        Bird chichken = new Bird("chicken");
+        Client ivan = new Client("Ivan");
+        Client peter = new Client("Peter");
+        Client anna  = new Client("Anna");
+        birdShop.buy(eagle,ivan,5, 10.0);
+        birdShop.buy(albatross,peter,3, 30.0);
+        birdShop.sell(eagle,anna,1,5.0);
+        birdShop.printTransactionList();
 
     }
 }
