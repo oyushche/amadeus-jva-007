03a9728f152c893ebf6bbb06de1a1d7d882be871
Kysliakovskyi <kostik-k@ukr.net>
2019-01-25 16:42:52 +0200

    2nd

--- src/com/amadeus/birds/Main.java
+++ src/com/amadeus/birds/Main.java
@@ -5,17 +5,24 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("=====  Bird shop ====");
-        BirdShopDemo birdShop = new BirdShopDemo();
+        BirdShop birdShopDemo = new BirdShop();
+
         Bird eagle = new Bird("eagle");
         Bird albatross = new Bird("albatross");
         Bird chichken = new Bird("chicken");
+
         Client ivan = new Client("Ivan");
         Client peter = new Client("Peter");
         Client anna  = new Client("Anna");
-        birdShop.buy(eagle,ivan,5, 10.0);
-        birdShop.buy(albatross,peter,3, 30.0);
-        birdShop.sell(eagle,anna,1,5.0);
-        birdShop.printTransactionList();
+
+        birdShopDemo.buy(eagle,ivan,5, 10.0);
+        birdShopDemo.buy(albatross,peter,3, 30.0);
+        birdShopDemo.sell(eagle,anna,1,5.0);
+
+        birdShopDemo.printTransactionList();
+
+        System.out.println("rest "+eagle+" = "+birdShopDemo.rest(eagle));
+        System.out.println("prifit "+eagle+" = "+birdShopDemo.profit(eagle));
 
     }
 }
